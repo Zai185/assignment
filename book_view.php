1c9fd1fa23ab32b7f479eb2ff6889d946c6b10b1
Zai185 <86978374+Zai185@users.noreply.github.com>
2022-12-05 10:34:51 +0630

    add card-bs to book_view.php and edit some style, add id to url from index.php

--- book_view.php
+++ book_view.php
@@ -1,25 +1,35 @@
 <?php
 session_start();
-
+include 'connection.php';
 $id = $_GET['id'];
 $query = "SELECT * FROM books WHERE id='$id'";
 $result = mysqli_query($conn, $query);
-if(!$result){
-    mysqli_connect_error();
+$book = mysqli_fetch_assoc($result);
+if ($result) {
+    $title = $book['title'];
+    $author = $book['author'];
+    $about = $book['about'];
+    $cover = $book['cover'];
 }
 ?>
 
-
-
 <?php include 'layout/header.php' ?>
 
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="<?php ?>" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
+<div class="card mx-auto" style="width: 50%; min-width: 380px; max-width: 50%;">
+    <img class="card-img-top" src="<?php echo $cover ?>" alt="Card image cap">
+    <div class="card-body">
+        <h5 class="card-title">Title:
+            <?php echo $title ?>
+        </h5>
+        <h5 class="card-title">Author:
+            <?php echo $author ?>
+        </h5>
+
+        <p class="card-text" style="height: 300px;overflow: scroll;">
+            <?php echo $about ?>
+        </p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
 </div>
 
 <?php include 'layout/footer.php' ?>
