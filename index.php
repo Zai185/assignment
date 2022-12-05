373b2207e632f337abbc6fab2e39209b5335bd54
Zai185 <86978374+Zai185@users.noreply.github.com>
2022-12-05 12:35:01 +0630

    minor change(move) from header to index(nav and modal)

--- index.php
+++ index.php
@@ -6,6 +6,77 @@ $result = mysqli_query($conn, $query);
 
 <?php include 'layout/header.php'; ?>
 
+<!-- nav and modal -->
+<nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php"><i class="bi bi-book me-1"></i>Multi-Library</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <?php if (isset($_SESSION['auth'])): ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        <a class="nav-link" style="cursor: pointer">Log Out</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item mx-1">
+                        <a href="book_create.php" class="btn btn-info rounded-pill my-auto"><i
+                                class="bi bi-plus-circle me-1"></i>Add Book</a>
+                    </li>
+                    <li class="nav-item user-item mx-1">
+                        <a class="btn btn-primary rounded-pill">
+                            <i class="bi bi-person-circle">
+                                <?php echo $_SESSION['uid'] ?></i>
+                        </a>
+                    </li>
+                    <?php else: ?>
+                    <li class="nav-item">
+                        <a class="nav-link " href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="register.php">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Log In</a>
+                    </li>
+                    <?php endif; ?>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- modal  -->
+    <!-- Button trigger modal -->
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    You Are Logging Out of from account "
+                    <?php echo $_SESSION['uid'] ?>", Are You Sure?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary"><a href="logout.php" class="text-white"
+                            style="text-decoration: none;">Confirm</a></button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!-- carousel section  -->
 <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
     <div class="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
@@ -73,7 +144,7 @@ $result = mysqli_query($conn, $query);
             <img class="book-img" src="<?php echo $book['cover'] ?>" alt=""
                 style="object-fit: contain; width: 100%; height: 100%;">
             <div class="book-textbox text-white text-bold text-center">
-                <a href="book_view.php?id=<?php echo $book['id']?>" style="color: white; text-decoration: none;">
+                <a href="book_view.php?id=<?php echo $book['id'] ?>" style="color: white; text-decoration: none;">
                     <h6>
                         <?php echo $book['title']; ?>
                     </h6>
